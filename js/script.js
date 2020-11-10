4914c345cb437ffe0d1ce0b8228151ec577d4a16
Sr <shooouuun@gmail.com>
2020-11-10 12:59:00 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,32 +1,38 @@
-var toggle__nav = document.querySelector(".page-header__trigger"),
-    nav = document.querySelector(".page-header__list");
-
-    toggle__nav.classList.remove("page-header__trigger--nojs");
-    nav.classList.remove("page-header__list--nojs");
-
-    toggle__nav.addEventListener("click", function (e) {
-      e.preventDefault();
-      nav.classList.toggle("page-header__list--open");
-      toggle__nav.classList.toggle("page-header__trigger--close");
-  });
-
-
 var toggle = document.querySelector(".main-nav__toggle");
-  var open = document.querySelector(".dropdown-menu__button--open");
-  var close = document.querySelector(".index-page");
+var open = document.querySelector(".dropdown-menu__button--open");
+var close = document.querySelector(".dropdown-menu__button--close");
+var themeButton = document.querySelector(".header__button");
+var currentTime = new Date().getHours();
+var theme = document.querySelector(".index-page");
+var titleBorder = document.querySelector(".intro__title");
 
-  toggle.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    open.classList.toggle("dropdown-menu__button--close1");
-    close.classList.toggle("dropdown-menu__button--open1");
-  });
+toggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  open.classList.toggle("dropdown-menu__button--close1");
+  close.classList.toggle("dropdown-menu__button--open1");
+});
 
-  var currentTime = new Date().getHours();
-  var theme = document.querySelector(".index-page");
-  function timeCheck() {
-      if (currentTime > 18 || currentTime < 6) {
-        theme.classList.add('dark');
-      }
+function timeCheck() {
+  if (currentTime > 18 || currentTime < 6) {
+    theme.classList.add('dark');
+    toggle.classList.add('dark__button');
+    themeButton.classList.add('dark__button');
+    titleBorder.classList.add('dark__border');
   };
+};
 
-  timeCheck();
+timeCheck();
+
+themeButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  theme.classList.toggle("dark");
+  toggle.classList.toggle('dark__button');
+  themeButton.classList.toggle('dark__button');
+  titleBorder.classList.toggle('dark__border');
+
+  if (themeButton.textContent === "White theme") {
+    themeButton.textContent = "Dark theme";
+  } else if (themeButton.textContent === "Dark theme") {
+    themeButton.textContent = "White theme";
+  };
+});
