895de56c42e4d68453b03f09f5cc331a4bdc1538
SHOUN <Shooouuun@gmail.com>
2020-12-10 12:43:50 +0300

    changing animation in navigation

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 var CURRENT_TIME  = new Date().getHours();
 var INDEX_PAGE = document.querySelector(".index-page");
+var INDEX_MAIN = document.querySelector(".index-main");
 var NAV_BTN = document.querySelector(".header__button");
 var TOGGLE_BTN_NAV = document.querySelector(".header__button-span--nav");
 var TOGGLE_BTN_CLOSE = document.querySelector(".header__button-span--close");
@@ -25,6 +26,7 @@ NAV_BTN.addEventListener("click", function (e) {
   e.preventDefault();
   TOGGLE_BTN_NAV.classList.toggle("header__button-span--close-mod");
   TOGGLE_BTN_CLOSE.classList.toggle("header__button-span--nav-mod");
+  INDEX_MAIN.classList.toggle("index-main--filter");
   INDEX_PAGE.classList.toggle("is-locked");
   NAV_WRAP.classList.toggle("header__list-main--open");
 });
@@ -37,10 +39,10 @@ THEME_BTN.addEventListener("click", function (e) {
 
   if (THEME_BTN.textContent === "White theme") {
     THEME_BTN.textContent = "Dark theme";
-    NAV_WRAP.style.backgroundColor = "white";
+    NAV_WRAP.style.backgroundColor = "rgba(240, 237, 232, 0.7)";
 
   } else if (THEME_BTN.textContent === "Dark theme") {
     THEME_BTN.textContent = "White theme";
-    NAV_WRAP.style.backgroundColor = "rgb(15 15 15)";
+    NAV_WRAP.style.backgroundColor = "rgba(15, 15, 15, 0.9)";
   };
 });
